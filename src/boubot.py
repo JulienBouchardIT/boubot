17193f560f2548896136ba70b581b25e48c95b50
Julien <julien.bouchard.it@gmail.com>
2020-10-06 23:33:27 -0400

    Restructure for @bot instead of @client

--- src/boubot.py
+++ src/boubot.py
@@ -7,19 +7,19 @@ from random import randrange
 import time
 import requests
 import os
+from discord.ext import commands
 
 #auth
-CLIENT_ID = os.environ['CLIENT_ID']
-CLIENT_SECRET = os.environ['CLIENT_SECRET']
 TOKEN = os.environ['TOKEN']
 
 CHANNEL_ID = os.environ['CHANNEL_ID']
 
-
-
-MSG_HELP = "asdasdasd"
+MSG_HELP = "pong        Response ping\r" \
+           "qwe\r" \
+           "asd\r"
 
 client = discord.Client()
+bot = commands.Bot(command_prefix='/', help_command=None)
 
 __games__ = []
 voice_channel = ""
@@ -36,93 +36,23 @@ def __get_gif__(key_word):
         return ''
 
 
-def get_rand_theme():
-    return random.choice(glob.glob("themes/*.json"))
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
 
 
-def set_theme(theme_json):
-    print(theme_json)
+@bot.command()
+async def help(ctx):
+    await ctx.send(MSG_HELP)
 
 
-@client.event
-async def on_message(message):
-
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('test'):
-        author = message.author
-        await message.channel.send('I heard you! {0.name}'.format(author))
-
-    # await message.add_reaction(emoji='\U0001F44D')
-
-    general_text = client.get_channel(CHANNEL_ID)
-    # general_vocal = client.get_channel(CHANNEL_ID)
+@bot.command()
+async def cat(ctx):
+    async with aiohttp.ClientSession() as session:
+        async with session.get('http://aws.random.cat/meow') as r:
+            if r.status == 200:
+                js = await r.json()
+                await ctx.send(js['file'])
 
 
-    if message.content.startswith('#gif'):
-        key_word = message.content[5:]
-        await general_text.send(__get_gif__(key_word))
-
-    if message.content.startswith('#help'):
-        await general_text.send(MSG_HELP)
-
-    if message.content.startswith('#hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
-        await general_text.send(msg)
-
-    # if message.content.startswith('#join'):
-    #    voice_channel = await general_vocal.connect()
-
-    if message.content.startswith('#leave'):
-        for vc in client.voice_clients:
-            if(vc.server == message.server):
-                await vc.disconnect()
-
-    if message.content.startswith('#newtheme'):
-        await general_text.send(str(get_rand_theme()))
-
-    if message.content.startswith('#add '):
-        __games__.append(message.content[5:])
-        await general_text.send("Done")
-
-    if message.content.startswith('#list'):
-        await general_text.send(str(__games__))
-
-    if message.content.startswith('#roll'):
-        await general_text.send(random.choice(__games__))
-
-    if message.content.startswith('#clear'):
-        __games__.clear()
-        await general_text.send("Done")
-
-    if message.content.startswith('#cat'):
-        async with aiohttp.ClientSession() as session:
-            async with session.get('http://aws.random.cat/meow') as r:
-                if r.status == 200:
-                    js = await r.json()
-                    await general_text.send(js['file'])
-
-    if message.content.startswith('#newpoll'):
-        time.sleep(10)
-        await general_text.send(str(message.reactions))
-
-
-@client.event
-async def on_member_join(member):
-    await member.create_dm()
-    await member.dm_channel.send(
-        f'Hi {member.name}, welcome to my Discord server!'
-    )
-
-
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
-
-
-client.run(TOKEN)
+bot.run(TOKEN)
